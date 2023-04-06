f6ce9a7b5a362a9337b0325bea42fe60cd0f7a14
santiagoGv0506 <129707742+santiagoGv0506@users.noreply.github.com>
2023-04-06 16:53:13 -0500

    playerPrefab

--- Assets/Scripts/GameInfo.cs
+++ Assets/Scripts/GameInfo.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class CheckPoint : MonoBehaviour
+public class GameInfo : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
