df742f58dc4d7fa093227b687215209a41904fd9
oscar03142 <oscarfernanoje@gmail.com>
2023-04-06 20:59:06 -0500

    comit10
    menu pausa terminado

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -8,19 +8,33 @@ public class MenuPausa : MonoBehaviour
 {
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.P)){
+            if(juegoPausado == true){
+                Reanudar();
+            }else{
+                Pausa();
+            }
+        }
+    }
     public void Pausa(){
+        juegoPausado = true;
         Time.timeScale =0f;
         botonPausa.SetActive(false);
         menuPausa.SetActive(true);
     }
 
     public void Reanudar(){
+        juegoPausado = false;
         Time.timeScale =1f;
         botonPausa.SetActive(true);
         menuPausa.SetActive(false);
     }
 
     public void Reiniciar(){
+        juegoPausado = false;
         Time.timeScale =1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
